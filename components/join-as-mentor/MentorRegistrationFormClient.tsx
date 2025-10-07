083347178006fe912876977b9632e8a843d28be8
ImanNasrEsfahani <iman.nasr.developer@gmail.com>
2025-10-06 17:40:37 -0700

    feat: Rename "Join as a Partner" to "Affiliate Registration Form" and update related links and translations

--- components/join-as-mentor/MentorRegistrationFormClient.tsx
+++ components/join-as-mentor/MentorRegistrationFormClient.tsx
@@ -243,7 +243,7 @@ export default function MentorRegistrationFormClient({ lang, translations }: Pro
               nameInput="birthDate"
               type="date"
               label={translations.birthDate}
-              required={''}
+              required={true}
               patternValue="(?:\d{1,2}[-/\s]\d{1,2}[-/\s]'?\d{2,4})|(?:\d{2,4}[-/\s]\d{1,2}[-/\s]\d{1,2})|(?:(?:January|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Sep|Oct|Nov|Dec)[\s-/,]*?\d{1,2}(?:\s)*(?:rd|th|st)?(?:\s)*[-/,]?(?:\s)*'?\d{2,4})|(?:\d{1,2}(?:\s)*(?:rd|th|st)?(?:\s)*(?:January|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sept|Sep|Oct|Nov|Dec)(?:\s)*?[-/,]?(?:\s)*'?\d{2,4})"
               patternMessage={translations.birthDateErrorMessage}
               placeholder={translations.birthDatePlaceholder}
@@ -318,7 +318,8 @@ export default function MentorRegistrationFormClient({ lang, translations }: Pro
                 nameTextArea="howDidYouKnowUs"
                 patternMessage=""
                 patternValue=""
-                required={translations.howDidYouKnowUsRequired}
+                // required={translations.howDidYouKnowUsRequired}
+                required=""
                 rows={3}
                 maxLength={1450}
                 maxLengthMessage={translations.howDidYouKnowUsErrorMessage}
