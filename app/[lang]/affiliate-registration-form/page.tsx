883969a0158320b92fa8dca2bccff698f1b425ce
ImanNasrEsfahani <iman.nasr.developer@gmail.com>
2025-10-20 17:10:41 -0700

    new photo for forms

--- app/[lang]/affiliate-registration-form/page.tsx
+++ app/[lang]/affiliate-registration-form/page.tsx
@@ -17,15 +17,25 @@ export default async function AffiliateFormPage({
   const { t } = getServerTranslation(lang, 'formComponent');
 
   return (
-    <div>
-      <Banner
-        image="/static/images/work-with-us/header.png"
-        title={t('partnerForm', { returnObjects: true }).banner}
-        lang={lang}
-      />
-      <div className="max-w-responsive mx-auto">
-        <JoinAsaPartnerForm lang={lang}/>
+    <>
+      <div className="hidden md:inline">
+        <Banner
+          image="/static/images/affiliate-registeration-form/header.png"
+          title={t('partnerForm', { returnObjects: true }).banner}
+          lang={lang}
+        />
       </div>
-    </div>
+      <div className="inline md:hidden">
+        <Banner
+          image="/static/images/affiliate-registeration-form/header-mobile.png"
+          title={t('partnerForm', { returnObjects: true }).banner}
+          lang={lang}
+        />
+      </div>
+     
+      <div className="max-w-responsive mx-auto">
+        <JoinAsaPartnerForm lang={lang} />
+      </div>
+    </>
   );
 }
