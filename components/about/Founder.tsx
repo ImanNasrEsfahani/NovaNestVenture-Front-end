14898b358a1caea9e0255326e171fe49bbd66721
ImanNasrEsfahani <iman.nasr.developer@gmail.com>
2025-11-17 13:33:04 -0800

    fix: Update founder section in About Us page for improved layout and readability, including text adjustments and button integration

--- components/about/Founder.tsx
+++ components/about/Founder.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import { getServerTranslation } from 'app/i18n';
+import ButtonRefactor from '@/components/common/ButtonRefactor';
 
 export default function Founder({ lang }: { lang: string }) {
   const { t } = getServerTranslation(lang, 'aboutUs');
   const founder = t('founder', { returnObjects: true }) as any;
+
+  const base = process.env.NEXT_PUBLIC_BASE_URL || "";
 
   return (
     <section className="w-full max-w-responsive mx-auto py-12 px-4 md:px-8 lg:px-16 my-16">
@@ -17,19 +20,28 @@ export default function Founder({ lang }: { lang: string }) {
                 alt={founder.image?.alt ?? 'Founder of NovaNest Venture - Rasoul Moradimehr'}
                 fill
                 sizes="(max-width: 1024px) 40vw, 240px"
-                className="object-cover object-center"
+                className="object-cover object-top "
               />
             </div>
           </div>
 
-          <div className="lg:col-span-2 flex flex-col justify-center gap-4 py-4">
-            <h3 className="text-xl md:text-2xl font-header font-bold mb-2">{founder.title}</h3>
-            <h4 className="text-lg md:text-xl font-semibold text-primary mb-6">{founder.name}</h4>
+          <div className="lg:col-span-2 flex flex-col justify-center gap-4 py-4 pr-6">
+            <h3 className="text-3xl md:text-5xl font-header font-bold">{founder.title}</h3>
+            <h4 className="text-xl md:text-2xl font-semibold text-primary mb-6">{founder.name}</h4>
 
-            <div className="text-sm md:text-base text-gray-700 space-y-4">
-              { founder.paragraphs.map((p: string, i: number) => (
-                    <p key={i} className="leading-relaxed">{p}</p>
-                ))}
+            <div className="text-base text-gray-700 space-y-4 mb-4">
+              {founder.paragraphs.map((p: string, i: number) => (
+                <p key={i} className="text-justify leading-relaxed">{p}</p>
+              ))}
+            </div>
+
+            <div className="w-full ml-auto md:w-[224px] p-2">
+              <ButtonRefactor
+                text="Read more"
+                type="link"
+                href={`${base}/en/profile/rasoul-moradimehr`}
+                bgColor="black"
+              />
             </div>
           </div>
         </div>
