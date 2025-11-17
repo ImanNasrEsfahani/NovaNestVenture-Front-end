692beaaa6326b701cb0c13a4d1b80c0ab5336830
ImanNasrEsfahani <iman.nasr.developer@gmail.com>
2025-11-17 11:35:52 -0800

    fix: Adjust positioning and sizing of images in PriorityCard component

--- components/home/PriorityCard.tsx
+++ components/home/PriorityCard.tsx
@@ -16,7 +16,7 @@ export default function PriorityCard({
         alt=""
         width={500}
         height={500}
-        className="text-darkGold bg-darkGold absolute bottom-2 -right-2 max-w-28 max-h-28 opacity-10 md:bottom-0"
+        className="text-darkGold bg-darkGold absolute bottom-0 right-0 max-w-28 max-h-28 opacity-10 md:bottom-0"
       />
 
       <div className="z-20 text-base text-center">{title}</div>
@@ -26,7 +26,7 @@ export default function PriorityCard({
         alt="NovaNest Venture Priority"
         width={500}
         height={500}
-        className="text-darkGold absolute left-2 top-2 max-w-16 max-h-16 lg:max-w-12 lg:max-h-12 z-10"
+        className="text-darkGold absolute left-2 top-2 max-w-12 max-h-12 lg:max-w-16 lg:max-h-16 z-10"
       />
     </div>
   );
