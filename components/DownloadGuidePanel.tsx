f17231b2b446756b51c4ffbb3c231591ec0933ac
ImanNasrEsfahani <iman.nasr.developer@gmail.com>
2025-11-18 15:55:53 -0800

    fix: update descriptions and titles for clarity in localization files

--- components/DownloadGuidePanel.tsx
+++ components/DownloadGuidePanel.tsx
@@ -80,7 +80,7 @@ export default function DownloadGuidePanel({
 
           <div className="h-full relative">
             <div className="max-h-auto w-auto absolute inset-0 bg-gray-400 rounded-2xl transform rotate-6 opacity-20" />
-            <div className="h-full min-h-96 bg-white/10 backdrop-blur-md rounded-2xl p-8 border rounded-lg border-white/20">
+            <div className="h-full min-h-96 bg-white/10 backdrop-blur-md rounded-2xl p-8 border border-white/20">
                 {thumbnail ? (
                   <Image
                     src={thumbnail}
